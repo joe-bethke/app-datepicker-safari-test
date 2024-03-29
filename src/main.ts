fa12385130239552ee39b8e1f4da450199fea1c9
joebethke <joe.bethke@2ndswing.com>
2024-03-29 11:56:29 -0500

    x

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,7 @@ import { customElement } from "lit/decorators.js";
 @customElement("my-component")
 class MyCompoonent extends LitElement {
   override render() {
-    return html`
-      <app-date-picker
-        locale="en-US"
-        .max=${"2024-04-31"}
-        .min=${"2024-03-38"}
-      ></app-date-picker>
-    `;
+    return html` <app-date-picker></app-date-picker> `;
   }
 }
 
